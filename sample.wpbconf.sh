e6c9e192ac8c877c23ee7355b2e71bbec268fe86
Paul <starsinmypockets@gmail.com>
2021-12-12 19:09:26 +0000

    Add conf

---
+++ sample.wpbconf.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+run_certbot=false # set to true to setup ssh certs
+sitedb_name=YOURSITEDBNME
+sitedb_pass=SITEDBPASS
+sitedb_user=SITEDBUSER
+sitedb_host=localhost
+site_host=SITEHOSTNAME
+wpb_template_path=/root/repos/wp-build/vhost_template.txt
