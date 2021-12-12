6f6e36b804e30bc27dd57cb4fa26d0c6a52077b3
Paul <starsinmypockets@gmail.com>
2021-12-12 18:47:28 +0000

    NGINX install initial

--- wpb.sh
+++ wpb.sh
@@ -10,10 +10,48 @@ installWPCLI() {
   fi
 }
 
-setupWP() {
-  wp core download
-  wp config --dbname=${sitedb_name} --dbuser=${sitedb_pass} --dbpass=${sitedb_pass} --dbhost=${sitedb_host}
+setupDB() {
+  if [ -f /root/.my.cnf ]; then
+    mysql -e "CREATE DATABASE ${sitedb_name} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -e "CREATE USER ${sitedb_user}@localhost IDENTIFIED BY '${sitedb_pass}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${sitedb_name}.* TO '${sitedb_user}'@'localhost';"
+    mysql -e "FLUSH PRIVILEGES;"
+  else
+    echo "Please configure /root/.my.cnf with your mysql credentials and try again"
+  fi
 }
 
-source $PWD/wpbbconf.sh
+setupWP() {
+  sudo -u www-data wp core download
+  sudo -u www-data wp config create --dbname=${sitedb_name} --dbuser=${sitedb_user} --dbpass=${sitedb_pass} --dbhost=${sitedb_host}
+}
+
+# do this first
+configureNGINX() {
+  echo "Hello world" > index.html
+  cp ${wpb_template_path} /etc/nginx/sites-available/${site_host}
+  sed -i "s|host|${site_host}|g" /etc/nginx/sites-available/${site_host}
+  ln -s /etc/nginx/sites-available/${site_host} /etc/nginx/sites-enabled/${site_host}
+  if nginx -t > /dev/null 2>&1; then
+    echo "NGINX syntax updated, restarting nginx..."
+    service nginx restart
+    echo "checking for site..."
+    if curl ${site_host} | grep "Hello world"; then
+      ## TODO prob want / need to test make sure site is up first
+      if runcertbot; then
+        certbot --nginx -d ${host} -d www.${host}
+      fi
+    else
+      echo "Site setup failed!"
+      exit 1
+    fi
+  else 
+    echo "syntax is not ok"
+  fi
+}
+
+source $PWD/wpbconf.sh
+configureNGINX
 installWPCLI
+setupDB
+setupWP
